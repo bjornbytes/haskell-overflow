1967d5472f2ad8d6802a60ab0ba2029811e1dc52
tie372 <tie.372@gmail.com>
2013-04-10 17:44:56 -0600

    Finished draft of BEncoding library, integrated with cabal.
    Added:
      -.gitignore
      -Setup.hs and overflow.cabal.
    Modified
      -overflow.hs
        -Has basic tests for BEncoding.
      -src/BEncode.hs
        -Added decode function, which is left as a debugging mechanism for now.
        -Added functions which parse the full BEncoding specification.  Some are not complete and need to take edge cases into account.

--- overflow.hs
+++ overflow.hs
@@ -1,1 +1,9 @@
 module Main where
+
+import BEncode
+
+main :: IO ()
+main = do
+  let x = (BList [(BInteger 4), (BInteger 6)])
+  print x
+  decode "d4:datal4:spam4:eggse5:bjorn7:swensone"
--- src/BEncode.hs
+++ src/BEncode.hs
@@ -1,2 +1,64 @@
 module BEncode where
 
+import Text.ParserCombinators.Parsec;
+import Data.Map;
+
+data BData = BString String
+           | BInteger Int
+           | BList [BData]
+           | BDictionary (Map BData BData)
+             deriving Eq
+
+instance Show BData where
+    show (BString s) = s
+    show (BInteger i) = (show i)
+    show (BList xs) = (show xs)
+    show (BDictionary m) = (show $ toList m)
+
+decode :: String -> IO ()
+decode s = case (parse bdata "" s) of
+             Left err -> print err
+             Right xs -> print xs
+
+bdata = bstring
+    <|> binteger
+    <|> blist
+    <|> bdictionary
+
+bstring :: Parser BData
+bstring = do
+  n <- number
+  char ':'
+  s <- count n anyChar
+  return $ BString s
+
+--TODO: Adhere more closely to the BEncoding spec.
+binteger :: Parser BData
+binteger = do
+  char 'i'
+  n <- number
+  char 'e'
+  return $ BInteger n
+
+blist :: Parser BData
+blist = do
+  char 'l'
+  xs <- many1 bdata
+  char 'e'
+  return $ BList xs
+
+bdictionary :: Parser BData
+bdictionary = do
+  char 'd'
+  xs <- many1 bdictionaryElement
+  char 'e'
+  return $ BDictionary (fromAscList xs)
+
+bdictionaryElement :: Parser (BData, BData)
+bdictionaryElement = do
+  s <- bstring
+  d <- bdata
+  return (s,d)
+
+number :: Parser Int
+number = do { n <- many1 digit; return $ read n }
