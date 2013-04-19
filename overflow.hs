ac8d1f3f920c2fd93c51a133a771556c1667f579
tie372 <tie.372@gmail.com>
2013-04-19 08:03:11 -0600

    Successfully parse list of ips/ports of peers from tracker response.

--- overflow.hs
+++ overflow.hs
@@ -1,13 +1,16 @@
 module Main where
 
 import qualified Metainfo as M
+import Peer
 import Announcer
 import Torrent
 import qualified Network.HTTP.Types.URI as U;
+import qualified Data.ByteString as B;
+import qualified Data.ByteString.Char8 as C;
+import Data.Char (ord)
 
 main :: IO ()
 main = do
   t <- torrentFromURL "http://cdimage.ubuntu.com/kubuntu/releases/quantal/release/kubuntu-12.10-desktop-amd64.iso.torrent"
-  announce $ announcer t
-  --print  $ U.urlEncode True $ infoHash $ metainfo t
-  
+  ps <- announce "started" $ announcer t
+  print $ ps
--- src/Announcer.hs
+++ src/Announcer.hs
@@ -1,12 +1,14 @@
 module Announcer where
 
 import BEncode
+import qualified Peer as P
 import Data.Maybe
 import Network.URI (parseURI)
 import Network.HTTP
 import qualified Network.HTTP.Types.URI as U
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
+import qualified Data.Map as M;
 
 data Announcer = Announcer {
   url :: B.ByteString,
@@ -19,12 +21,34 @@ data Announcer = Announcer {
   left :: Int
 }
 
-announce :: Announcer -> IO ()
-announce a = do
-  C.putStrLn $ announceRequest a ""
-  h <- simpleHTTP . defaultGETRequest_ . fromJust . parseURI $ C.unpack $ announceRequest a ""
+announce :: String -> Announcer -> IO [P.Peer]
+announce event a = do
+  putStrLn $ announceRequest event a
+  h <- simpleHTTP $ defaultGETRequest_ $ fromJust $ parseURI $ announceRequest event a
   response <- getResponseBody h
-  C.putStrLn response
+  return $ parsePeers $ assumeBString $ fromJust $ M.lookup "peers" $ decodeBDictionary response
 
-announceRequest :: Announcer -> String -> B.ByteString
-announceRequest a event = B.append (url a) $ B.concat [C.pack "?info_hash=", U.urlEncode True $ infoHash a, C.pack "&peer_id=", U.urlEncode True $ peerId a, C.pack "&port=", C.pack $ show $ localPort a, C.pack "&uploaded=", C.pack $ show $ uploaded a, C.pack "&downloaded=", C.pack $ show $ downloaded a, C.pack "&left=", C.pack $ show $ left a, C.pack "&compact=1"]
+announceRequest :: String -> Announcer -> String
+announceRequest event a = C.unpack $ B.append (url a) $ B.concat
+                      [ C.pack "?info_hash=", U.urlEncode True $ infoHash a
+                      , C.pack "&peer_id=", U.urlEncode True $ peerId a
+                      , C.pack "&port=", C.pack $ show $ localPort a
+                      , C.pack "&uploaded=", C.pack $ show $ uploaded a
+                      , C.pack "&downloaded=", C.pack $ show $ downloaded a
+                      , C.pack "&left=", C.pack $ show $ left a
+                      , C.pack "&compact=1" ]
+
+parsePeers :: B.ByteString -> [P.Peer]
+parsePeers str = case B.length str of
+                   0 -> []
+                   otherwise -> p:(parsePeers rest)
+                                   where p = P.Peer {
+                                               P.ip = C.unpack $ B.take 4 piece,
+                                               P.port = ((fromIntegral $ B.head $ B.drop 4 piece) * 256) + (fromIntegral $ B.head $ B.drop 5 piece),
+
+                                               P.interested = False,
+                                               P.interesting = False,
+                                               P.choked = True,
+                                               P.choking = True
+                                             }
+                                         (piece, rest) = B.splitAt 6 str
