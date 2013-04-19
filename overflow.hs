90daff0b4356f8edbe04919754b4b4c88956f91c
tie372 <tie.372@gmail.com>
2013-04-19 09:25:25 -0600

    Basic structure for TCP listen socket is in place.

--- overflow.hs
+++ overflow.hs
@@ -5,6 +5,7 @@ import qualified Metainfo as M
 import Peer
 import Announcer
 import Torrent
+import Network
 import qualified Network.HTTP.Types.URI as U;
 import qualified Data.ByteString as B;
 import qualified Data.ByteString.Char8 as C;
@@ -19,3 +20,13 @@ main :: IO ()
 main = do
   t <- torrentFromURL "http://cdimage.ubuntu.com/kubuntu/releases/quantal/release/kubuntu-12.10-desktop-amd64.iso.torrent"
   putStrLn $ "Downloading " ++ (C.unpack $ assumeBString $ fromJust $ Map.lookup "name" $ M.info $ metainfo t) ++ " [" ++ (show $ assumeBInteger $ fromJust $ Map.lookup "length" $ M.info $ metainfo t) ++ " bytes]..."
+  sock <- listenOn $ PortNumber 6888
+  putStrLn "Listening on 6888..."
+  socketAccept sock
+
+socketAccept :: Socket -> IO ()
+socketAccept sock = do
+  (handle, ip, port) <- accept sock
+  putStrLn $ "New connection from " ++ (show ip) ++ ":" ++ (show port)
+  --Do stuff
+  socketAccept sock
